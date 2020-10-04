8e8dee3a1539131708dac8a439f326b91f0ab60a
nicolasdoenerundpizzahaus <nicolas.wild@tech11.com>
2020-10-05 00:39:04 +0200

    test2

--- rejectcookies.js
+++ rejectcookies.js
@@ -1,1 +1,5 @@
-document.body.style.background = "blue";
+document.body.style.background = "yellow";
+
+var x = document.getElementsByTagName("button");
+x[0].innerHTML = "Hello World!";
+x[0].style.background = "blue"
